0df5c70da75995ba3079b4d15e3fabd64e47c43a
twarped <samthebeastofall@gmail.com>
2021-04-06 16:40:11 -0600

    placeholder

--- test.sh
+++ test.sh
@@ -2,17 +2,17 @@
 
 status=$(git status)
 
-echo "$status"
+#echo "$status"
 
 line_count=$(($(printf "%s\n" "$status" | wc -l) - 1))
 
-echo "$line_count"
+#echo "$line_count"
 
 if (($line_count >= 7)); then
   echo "need to push"
 	git add .
 	git commit -m "placeholder"
 	git push origin main
-else
-  echo "no need to push"
+#else
+  #echo "no need to push"
 fi
