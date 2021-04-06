2b75bd4d4856dd1c16737e25c299b67fcb8268b2
twarped <samthebeastofall@gmail.com>
2021-04-06 16:55:59 -0600

    placeholder

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+while true
+do
+
 status=$(git status)
 
 echo "$status"
@@ -19,3 +22,5 @@ if [["$status" == *"Your branch is up to date with"*]]; then
   echo "need to pull"
 	git pull origin main
 fi
+
+done
