8abe8e192148c11be6c4c75fc596d34fa7bb733b
twarped <samthebeastofall@gmail.com>
2021-04-07 15:25:08 -0600

    placeholder

--- test.sh
+++ test.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-while true
-do
+#while true
+#do
 
 status=$(git status)
 
-#echo "$status"
+echo "$status"
 
 line_count=$(($(printf "%s\n" "$status" | wc -l) - 1))
 
 #echo "$line_count"
 
-if (($line_count >= 7)); then
+if [[ "$status" == *"behind"* ]]; then
+  echo "need to pull"
+	git pull origin main
+fi
+
+if [[ "$status" == *"committed"* && "$status" == *"modified:"* ]]; then
   echo "need to push"
 	git add .
 	git commit -m "placeholder"
 	git push origin main
 fi
 
-if [["$status" == *"Your branch is up to date with"*]]; then
-  echo "need to pull"
-	git pull origin main
-fi
-
-done
+#done
