f389f67c77d3df9aa2fc4d777cd97271b89d1b1d
twarped <samthebeastofall@gmail.com>
2021-04-08 16:50:27 -0600

    placeholder

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
 var ejs = require('ejs');
+var localtunnel = require('localtunnel');
 
 var app = express();
 
@@ -13,6 +9,18 @@ app.configure(function(){
   app.use(express.static(__dirname, 'public'));
   app.engine("html", ejs.renderFile)
 });
+
+(async () => {
+  const tunnel = await localtunnel({ port: 3000 });
+
+  // the assigned public url for your tunnel
+  // i.e. https://abcdefgjhij.localtunnel.me
+  console.log(tunnel.url);
+
+  tunnel.on('close', () => {
+    console.log('tunnels are closed...');
+  });
+})();
 
 app.get('/', (req, res) => {
   res.render(__dirname+"/views/index.html");
